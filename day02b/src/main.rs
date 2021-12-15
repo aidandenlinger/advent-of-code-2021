676b7d3256fb466dc8432b7f6f844ade88e45938
Aidan Denlinger <adenling@ucsd.edu>
2021-12-14 17:47:07 -0800

    day02b

---
+++ day02b/src/main.rs
@@ -0,0 +1,97 @@
+use std::{error::Error, fs};
+
+fn main() {
+    // create the struct for our location
+    let mut l = Location::new();
+
+    let updates = parse_update_strs(&fs::read_to_string("input.txt").unwrap());
+
+    l.update_vec(updates);
+
+    println!("{}", l.mult())
+}
+
+#[derive(Debug)]
+enum Update {
+    Forward(i32),
+    Up(i32),
+    Down(i32),
+}
+
+/// Parse a string of updates into a vector of updates
+fn parse_update_strs(i: &str) -> Vec<Update> {
+    i.lines().map(|i| parse_update_str(i).unwrap()).collect()
+}
+
+/// Turn a single update string into an Update
+fn parse_update_str(i: &str) -> Result<Update, Box<dyn Error>> {
+    match i.split_whitespace().collect::<Vec<_>>()[..] {
+        ["forward", i] => Ok(Update::Forward(i.parse().unwrap())),
+        ["down", i] => Ok(Update::Down(i.parse().unwrap())),
+        ["up", i] => Ok(Update::Up(i.parse().unwrap())),
+        _ => Err("incorrect instruction format".into()),
+    }
+}
+
+/// Hold the current location
+#[derive(Debug)]
+struct Location {
+    horiz: i32,
+    vert: i32,
+    aim: i32,
+}
+
+impl Location {
+    /// Starting location of 0,0
+    fn new() -> Location {
+        Location {
+            horiz: 0,
+            vert: 0,
+            aim: 0,
+        }
+    }
+
+    /// Update the location by one Update
+    fn update(&mut self, u: &Update) {
+        match u {
+            Update::Forward(i) => {
+                self.horiz += i;
+                self.vert += self.aim * i
+            }
+            Update::Down(i) => self.aim += i,
+            Update::Up(i) => self.aim -= i,
+        }
+    }
+
+    /// Update with a vector of updates
+    fn update_vec(&mut self, updates: Vec<Update>) {
+        for u in updates.iter() {
+            self.update(u)
+        }
+    }
+
+    /// Mult the two locational values together
+    fn mult(&self) -> i32 {
+        self.horiz * self.vert
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    /// The sample input from the website
+    #[test]
+    fn test_case() {
+        use super::*;
+        let updates = "\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+        let mut l = Location::new();
+        l.update_vec(parse_update_strs(updates));
+        assert_eq!(900, l.mult());
+    }
+}
